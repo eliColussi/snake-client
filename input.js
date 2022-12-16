d978aa004edba95ad89a0dd864643e305aa99b8d
eli <eliitheboss@gmail.com>
2022-12-16 02:00:04 +0000

    updated to allow user input on snake

--- input.js
+++ input.js
@@ -1,4 +1,10 @@
-const setupInput = function (conn) {
+const { createConnection } = require("net");
+
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +17,19 @@ const handleUserInput = function(data) {
   if (data === "\u0003") {
     process.exit();
   }
+  if (data === "w" || data === "W") {
+    connection.write("Move: up");
+  }
+  if (data === "a" || data === "A") {
+    connection.write("Move: left");  
+  }
+  if (data === "s" || data === "S") {
+    connection.write("Move: down");
+  }
+  if (data === "d" || data === "D") {
+    connection.write("Move: right");
+  }
+
 };
 
 
