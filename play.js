fc53d4a5294cbc2fa95907d9ab0a469fe50cadeb
eli <eliitheboss@gmail.com>
2022-12-16 01:15:13 +0000

    adding user input

--- play.js
+++ play.js
@@ -1,2 +1,18 @@
 const {connect} = require("./client");
- 
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+}; 
+
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
