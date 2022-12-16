ea2fbe342a1f16c9394cc01aeded68b09ca83615
eli <eliitheboss@gmail.com>
2022-12-16 04:32:42 +0000

    seperate file for constant variables

--- constants.js
+++ constants.js
@@ -1,5 +1,4 @@
-// seperate file for constants to easily tweak and clean up code
-
+// seperate file for constant variables to easily tweak and clean up code
 const IP = "localhost";
 const PORT = 50541;
 
